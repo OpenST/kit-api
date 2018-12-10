7e73b19b9a1adc2a54fea53dfb971e7a2a9cfa4f
Puneet Khushwani <puneet.khushwani@ost.com>
2018-12-10 15:08:32 +0530

    base service and related files

--- app/services/services_base.rb
+++ app/services/services_base.rb
@@ -20,10 +20,29 @@ class ServicesBase
     @params = HashWithIndifferentAccess.new(permitted_params)
   end
 
-  def self.get_service_params(service_class)
-    # Load mandatory params yml only once
-    @mandatory_params ||= YAML.load_file(open(Rails.root.to_s + '/app/services/service_params.yml'))
-    @mandatory_params[service_class]
+  # Handle Error & Exceptions
+  #
+  # * Author: Puneet
+  # * Date: 06/12/2018
+  # * Reviewed By:
+  #
+  # @return [Result::Base]
+  #
+  def handle_errors_and_exceptions(&block)
+    begin
+      block.call
+    rescue OstCustomError => ose
+      puts "Handled Exception: #{ose.response}"
+      return ose.response
+    rescue StandardError => se
+      puts "Unhandled Exception: msg: #{se.message}"
+      puts "Unhandled Exception: trace: #{se.backtrace}"
+      return exception_with_data(
+          se,
+          's_b_1',
+          GlobalConstant::ErrorAction.default
+      )
+    end
   end
 
   def current_time
@@ -32,6 +51,12 @@ class ServicesBase
 
   def current_timestamp
     @c_tstmp ||= current_time.to_i
+  end
+
+  def self.get_service_params(service_class)
+    # Load mandatory params yml only once
+    @mandatory_params ||= YAML.load_file(open(Rails.root.to_s + '/app/services/service_params.yml'))
+    @mandatory_params[service_class]
   end
 
   private
@@ -47,7 +72,7 @@ class ServicesBase
       missing_mandatory_params_errors << "missing_#{mandatory_param}" if @params[mandatory_param].to_s.blank?
     end if service_params_list[:mandatory].present?
 
-    return validation_error(
+    fail OstCustomError.new validation_error(
         'sb_1',
         'invalid_api_params',
         missing_mandatory_params_errors,
