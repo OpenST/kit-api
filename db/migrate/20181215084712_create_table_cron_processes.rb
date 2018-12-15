2123b8d5f0e5bfec0bb612043b4637558ce84120
alpeshvmodi <alpeshvmodi@gmail.com>
2018-12-15 14:19:32 +0530

    cron processes migration.

---
+++ db/migrate/20181215084712_create_table_cron_processes.rb
@@ -0,0 +1,22 @@
+class CreateTableCronProcesses < DbMigrationConnection
+  def up
+    run_migration_for_db(EstablishSaasConfigDbConnection) do
+      create_table :cron_processes do |t|
+        t.column :kind, :tinyint, limit: 1, null: false
+        t.column :ip_address, :string, limit: 255, null: false
+        t.column :group_id, :string, limit: 255, :null => true
+        t.column :params, :text, null: true
+        t.column :status, :tinyint, limit: 1, null: false
+        t.column :last_start_time, :datetime, null: true
+        t.column :last_end_time, :datetime, null: true
+        t.timestamps
+      end
+    end
+  end
+
+  def down
+    run_migration_for_db(EstablishSaasConfigDbConnection) do
+      drop_table :cron_processes
+    end
+  end
+end
