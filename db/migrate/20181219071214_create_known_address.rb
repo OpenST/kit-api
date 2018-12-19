e92f3c57ddab30708f8e8f669317d6e5d3951586
Puneet Khushwani <puneet.khushwani@ost.com>
2018-12-19 13:45:15 +0530

    added new migrations

--- db/migrate/20181219071214_create_known_address.rb
+++ db/migrate/20181219071214_create_known_address.rb
@@ -1,19 +1,30 @@
 class CreateKnownAddress < DbMigrationConnection
 
   def up
+
     run_migration_for_db(EstablishSaasBigDbConnection) do
+
       create_table :known_addresses do |t|
         t.column :client_id, :integer, null: true
         t.column :encryption_salt_id, :integer, null: true
         t.column :address_type, :tinyint, null: false, limit: 1
-        t.column :uuid, :string, null: false
-        t.column :name, :string
+        t.column :uuid, :string, null: false #TODO: do we need uuid ?
+        t.column :name, :string, limit: 255, null: true
         t.column :ethereum_address, :string, limit: 255
         t.column :private_key, :text
         t.column :status, :tinyint, null: false, limit: 1
         t.timestamps
       end
+
+
+      add_index :known_addresses, [:client_id, :uuid], name: 'uk_1', unique: true #TODO: do we need this ?
+      add_index :known_addresses, [:encryption_salt_id], name: 'uk_2', unique: true
+      add_index :known_addresses, [:ethereum_address], name: 'uk_3', unique: true
+
+      execute ("ALTER TABLE managed_addresses AUTO_INCREMENT = 70000")
+
     end
+
   end
 
   def down
