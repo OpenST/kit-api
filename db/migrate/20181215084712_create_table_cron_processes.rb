824c851cd885967247b2ff3568ecb1533d197869
Kedar Chandrayan <kedarchandrayan@gmail.com>
2018-12-20 14:00:16 +0530

    minor.

--- db/migrate/20181215084712_create_table_cron_processes.rb
+++ db/migrate/20181215084712_create_table_cron_processes.rb
@@ -1,6 +1,6 @@
 class CreateTableCronProcesses < DbMigrationConnection
   def up
-    run_migration_for_db(DbConnection::KitSaasSubenv) do
+    run_migration_for_db(DbConnection::SaasSubenv) do
       create_table :cron_processes do |t|
         t.column :kind, :tinyint, limit: 1, null: false
         t.column :ip_address, :string, limit: 255, null: false
@@ -15,7 +15,7 @@ class CreateTableCronProcesses < DbMigrationConnection
   end
 
   def down
-    run_migration_for_db(DbConnection::KitSaasSubenv) do
+    run_migration_for_db(DbConnection::SaasSubenv) do
       drop_table :cron_processes
     end
   end
