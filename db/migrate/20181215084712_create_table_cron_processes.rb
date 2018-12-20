9d4b92e4ca37eae23f72a8e0b864a58dea5e0e09
Kedar Chandrayan <kedarchandrayan@gmail.com>
2018-12-20 13:53:09 +0530

    db change for cron process table

--- db/migrate/20181215084712_create_table_cron_processes.rb
+++ db/migrate/20181215084712_create_table_cron_processes.rb
@@ -1,21 +1,21 @@
 class CreateTableCronProcesses < DbMigrationConnection
   def up
-    run_migration_for_db(DbConnection::ConfigSubenv) do
+    run_migration_for_db(DbConnection::KitSaasSubenv) do
       create_table :cron_processes do |t|
         t.column :kind, :tinyint, limit: 1, null: false
         t.column :ip_address, :string, limit: 255, null: false
         t.column :chain_id, :string, limit: 255, :null => true
         t.column :params, :text, null: true
         t.column :status, :tinyint, limit: 1, null: false
-        t.column :last_start_time, :datetime, null: true
-        t.column :last_end_time, :datetime, null: true
+        t.column :last_started_at, :datetime, null: true
+        t.column :last_ended_at, :datetime, null: true
         t.timestamps
       end
     end
   end
 
   def down
-    run_migration_for_db(DbConnection::ConfigSubenv) do
+    run_migration_for_db(DbConnection::KitSaasSubenv) do
       drop_table :cron_processes
     end
   end
