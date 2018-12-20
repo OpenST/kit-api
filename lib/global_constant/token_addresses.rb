677f1fe5de9218b6ccb8b4165a9e704e691ddb17
Dhananjay8 <dhananjay@ost.com>
2018-12-20 21:28:02 +0530

    global constants for token addresses.

---
+++ lib/global_constant/token_addresses.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+module GlobalConstant
+
+  class TokenAddresses
+
+    class << self
+
+      def aux
+        'aux'
+      end
+
+      def origin
+        'origin'
+      end
+
+      def gateway
+        'gateway'
+      end
+
+      def co_gateway
+        'co_gateway'
+      end
+
+      def ubt
+        'ubt'
+      end
+
+      def owner
+        'owner'
+      end
+
+      def funder
+        'funder'
+      end
+
+    end
+
+  end
+
+end
