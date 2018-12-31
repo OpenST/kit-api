c21dc74f83cdcffcf2ecd5899fa099e435d78553
Puneet Khushwani <puneet.khushwani@ost.com>
2018-12-31 11:43:07 +0530

    migration change

--- db/migrate/20181219123610_create_chain_address.rb
+++ db/migrate/20181219123610_create_chain_address.rb
@@ -2,7 +2,7 @@ class CreateChainAddress < DbMigrationConnection
 
   def up
 
-    run_migration_for_db(DbConnection::KitSaasSubenv) do
+    run_migration_for_db(DbConnection::SaasSubenv) do
 
       create_table :chain_addresses do |t|
         t.column :chain_id, :integer, null: false
@@ -22,8 +22,8 @@ class CreateChainAddress < DbMigrationConnection
   end
 
   def down
-    run_migration_for_db(DbConnection::KitSaasSubenv) do
-      drop_table :chain_addresses if DbConnection::KitSaasSubenv.connection.table_exists? :chain_addresses
+    run_migration_for_db(DbConnection::SaasSubenv) do
+      drop_table :chain_addresses if DbConnection::SaasSubenv.connection.table_exists? :chain_addresses
     end
   end
   
