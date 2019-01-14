20931232a6ba2aca2a807d2d87fbbb6634667492
Puneet Khushwani <puneet.khushwani@ost.com>
2019-01-14 13:34:33 +0530

    added workflow step model

---
+++ app/models/workflow_step.rb
@@ -0,0 +1,24 @@
+class WorkflowStep < DbConnection::KitSaasSubenv
+  
+  enum kind: {
+      GlobalConstant::WorkflowStep.economy_setup_init_kind => 1,
+      GlobalConstant::WorkflowStep.generate_token_addresses_kind => 2,
+      GlobalConstant::WorkflowStep.deploy_origin_branded_token_kind => 3,
+      GlobalConstant::WorkflowStep.deploy_origin_branded_token_kind => 4,
+      GlobalConstant::WorkflowStep.deploy_aux_token_organization_kind => 5,
+      GlobalConstant::WorkflowStep.deploy_utility_branded_token_kind => 6,
+      GlobalConstant::WorkflowStep.token_deploy_gateway_kind => 7,
+      GlobalConstant::WorkflowStep.token_deploy_co_gateway_kind => 8,
+      GlobalConstant::WorkflowStep.set_co_gateway_in_ubt_kind => 9,
+      GlobalConstant::WorkflowStep.set_gateway_in_bt_kind => 10
+  }
+
+  enum status: {
+    GlobalConstant::WorkflowStep.queued_status => 1,
+    GlobalConstant::WorkflowStep.pending_status => 2,
+    GlobalConstant::WorkflowStep.processed_status => 3,
+    GlobalConstant::WorkflowStep.failed_status => 4,
+    GlobalConstant::WorkflowStep.timeout_status => 5
+  }
+
+end
---
+++ lib/global_constant/workflow_step.rb
@@ -0,0 +1,71 @@
+module GlobalConstant
+
+  class WorkflowStep
+
+    class << self
+
+      def queued_status
+        'queued'
+      end
+
+      def pending_status
+        'pending'
+      end
+
+      def processed_status
+        'processed'
+      end
+
+      def failed_status
+        'failed'
+      end
+
+      def timeout_status
+        'timeOut'
+      end
+
+      def economy_setup_init_kind
+        'economySetupInit'
+      end
+
+      def generate_token_addresses_kind
+        'generateTokenAddresses'
+      end
+
+      def deploy_origin_token_organization_kind
+        'deployOriginTokenOrganization'
+      end
+
+      def deploy_aux_token_organization_kind
+        'deployAuxTokenOrganization'
+      end
+
+      def deploy_origin_branded_token_kind
+        'deployOriginBrandedToken'
+      end
+
+      def deploy_utility_branded_token_kind
+        'deployUtilityBrandedToken'
+      end
+
+      def token_deploy_gateway_kind
+        'tokenDeployGateway'
+      end
+
+      def token_deploy_co_gateway_kind
+        'tokenDeployCoGateway'
+      end
+
+      def set_co_gateway_in_ubt_kind
+        'setCoGatewayInUbt'
+      end
+
+      def set_gateway_in_bt_kind
+        'setGatewayInBt'
+      end
+
+    end
+
+  end
+
+end
