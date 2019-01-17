272e081c0cbfa58ee74fed8f33f2ffda16003ba5
ankitsingh <ankit@ost.com>
2019-01-17 17:47:48 +0530

    model for workflow table

---
+++ app/models/workflow.rb
@@ -0,0 +1,15 @@
+class Workflow < DbConnection::KitSaasSubenv
+
+  enum kind: {
+    GlobalConstant::Workflow.token_deploy => 1,
+    GlobalConstant::Workflow.stake_and_mint => 2,
+    GlobalConstant::Workflow.state_root_sync => 3
+  }
+
+  enum status: {
+    GlobalConstant::Workflow.in_progress => 1,
+    GlobalConstant::Workflow.completed => 2,
+    GlobalConstant::Workflow.failed => 3,
+    GlobalConstant::Workflow.failed_complete => 4
+  }
+end
