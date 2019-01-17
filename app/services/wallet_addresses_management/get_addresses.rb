c604a5ff49882949f5a23fd45c877f9ee17ae350
ankitsingh <ankit@ost.com>
2019-01-17 19:02:15 +0530

    GET token addresses api

--- app/services/wallet_addresses_management/get_addresses.rb
+++ app/services/wallet_addresses_management/get_addresses.rb
@@ -33,15 +33,6 @@ module WalletAddressesManagement
 
       handle_errors_and_exceptions do
 
-        #Todo: Remove following hardcoded entries. This is only to unblock front-end
-        @origin_addresses = {owner: '0x96680082E040b81d7fd8a7c6d0610c6F98176ac8', admin: '', whitelisted: ['0x96680082E040b81d7fd8a7c6d0610c6F98176ac8'], workers: []}
-        @auxiliary_addresses = {owner: '0x96680082E040b81d7fd8a7c6d0610c6F98176ac8', admin: '', whitelisted: ['0x96680082E040b81d7fd8a7c6d0610c6F98176ac8'], workers: []}
-        @sign_messages = {wallet_association:GlobalConstant::MessageToSign.wallet_association}
-
-        @api_response_data[:origin_addresses] = @origin_addresses
-        @api_response_data[:auxiliary_addresses] = @auxiliary_addresses
-        @api_response_data[:sign_messages] = @sign_messages
-
         #r = validate_and_sanitize
         #return r unless r.success?
 
