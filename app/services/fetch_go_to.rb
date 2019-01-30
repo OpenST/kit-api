460995a2c632bfd43c328c3b4a78183d045d3e47
ankitsingh <ankit@ost.com>
2019-01-30 18:09:06 +0530

    bug fix

--- app/services/fetch_go_to.rb
+++ app/services/fetch_go_to.rb
@@ -38,7 +38,7 @@ class FetchGoTo < ServicesBase
 
       return GlobalConstant::GoTo.verify_email if @manager[:properties].exclude?(GlobalConstant::Manager.has_verified_email_property)
 
-      if @is_password_auth_cookie_valid
+      if @is_multi_auth_cookie_valid
 
         fetch_by_economy_state
 
