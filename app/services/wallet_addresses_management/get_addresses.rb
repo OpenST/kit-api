0c5de56a65c234ff3b25fec36680779be137d635
ankitsingh <ankit@ost.com>
2019-02-01 15:52:10 +0530

    added sub env payloads in token address get api

--- app/services/wallet_addresses_management/get_addresses.rb
+++ app/services/wallet_addresses_management/get_addresses.rb
@@ -36,6 +36,9 @@ module WalletAddressesManagement
         r = validate_and_sanitize
         return r unless r.success?
 
+        r = fetch_sub_env_payloads
+        return r unless r.success?
+
         r = fetch_addresses
         return r unless r.success?
 
@@ -91,26 +94,58 @@ module WalletAddressesManagement
     end
 
 
+    # fetch token addresses
+    #
+    # * Author: Ankit
+    # * Date: 15/01/2019
+    # * Reviewed By:
+    #
+    # @return [Result::Base]
+    #
     def fetch_addresses
       @token_id = @token_details[:id]
       data_from_cache = KitSaasSharedCacheManagement::TokenAddresses.new([@token_id]).fetch
       success_with_data(data_from_cache)
     end
 
+    # fetch the sub env response data entity
+    #
+    # * Author: Ankit
+    # * Date: 01/02/2019
+    # * Reviewed By:
+    #
+    # @return [Result::Base]
+    #
+    def fetch_sub_env_payloads
+      r = SubEnvPayload.new({client_id:@client_id}).perform
+      return r unless r.success?
+
+      @sub_env_payload_data = r.data[:sub_env_payloads]
+
+      success
+    end
+
     def format_response_data(addresses_data)
-      addresses = {}
-      addresses[:whitelisted] = addresses_data[@token_id][GlobalConstant::TokenAddresses.owner_address_kind] ||= []
-      addresses[:workers] = addresses_data[@token_id][GlobalConstant::TokenAddresses.worker_address_kind] ||= []
-      addresses[:owner] = addresses_data[@token_id][GlobalConstant::TokenAddresses.owner_address_kind]
-      addresses[:admin] = addresses_data[@token_id][GlobalConstant::TokenAddresses.admin_address_kind]
+      origin_addresses = {}
+      origin_addresses[:whitelisted] = addresses_data[@token_id][GlobalConstant::TokenAddresses.owner_address_kind] ||= []
+      origin_addresses[:workers] = addresses_data[@token_id][GlobalConstant::TokenAddresses.origin_worker_address_kind] ||= []
+      origin_addresses[:owner] = addresses_data[@token_id][GlobalConstant::TokenAddresses.owner_address_kind]
+      origin_addresses[:admin] = addresses_data[@token_id][GlobalConstant::TokenAddresses.origin_admin_address_kind]
+
+      auxiliary_addresses = {}
+      auxiliary_addresses[:whitelisted] = addresses_data[@token_id][GlobalConstant::TokenAddresses.owner_address_kind] ||= []
+      auxiliary_addresses[:workers] = addresses_data[@token_id][GlobalConstant::TokenAddresses.aux_worker_address_kind] ||= []
+      auxiliary_addresses[:owner] = addresses_data[@token_id][GlobalConstant::TokenAddresses.owner_address_kind]
+      auxiliary_addresses[:admin] = addresses_data[@token_id][GlobalConstant::TokenAddresses.aux_admin_address_kind]
 
       sign_messages = {
         wallet_association: GlobalConstant::MessageToSign.wallet_association
       }
 
-      @api_response_data[:origin_addresses] = addresses
-      @api_response_data[:auxiliary_addresses] = addresses
+      @api_response_data[:origin_addresses] = origin_addresses
+      @api_response_data[:auxiliary_addresses] = auxiliary_addresses
       @api_response_data[:sign_messages] = sign_messages
+      @api_response_data[:sub_env_payloads] = @sub_env_payload_data
 
       success
 
