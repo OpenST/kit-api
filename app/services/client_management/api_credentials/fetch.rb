759e1284ebd9f6ce8160cd12d2a6da4a941393a5
ankitsingh <ankit@ost.com>
2019-02-04 14:06:29 +0530

    temp commit to unblock FE

--- app/services/client_management/api_credentials/fetch.rb
+++ app/services/client_management/api_credentials/fetch.rb
@@ -29,9 +29,47 @@ module ClientManagement
       #
       def perform
         #temporary changes to unblock FE. 4Feb19
-        success_with_data({}
+        fetch_token_details
+
+        fetch_sub_env_payloads
+
+        success_with_data({
+                            token: @token,
+                            sub_env_payloads: @sub_env_payload_data
+                          }
           #api_credentials: KitSaasSharedCacheManagement::ApiCredentials.new([@client_id]).fetch[@client_id]
         )
+      end
+
+      # Fetch token details
+      #
+      #
+      # * Author: Ankit
+      # * Date: 19/01/2019
+      # * Reviewed By:
+      #
+      # @return [Result::Base]
+      def fetch_token_details
+        @token = KitSaasSharedCacheManagement::TokenDetails.new([@client_id]).fetch[@client_id] || {}
+
+        success
+      end
+
+      # fetch the sub env response data entity
+      #
+      # * Author: Ankit
+      # * Date: 01/02/2019
+      # * Reviewed By:
+      #
+      # @return [Result::Base]
+      #
+      def fetch_sub_env_payloads
+        r = SubEnvPayload.new({client_id:@client_id}).perform
+        return r unless r.success?
+
+        @sub_env_payload_data = r.data[:sub_env_payloads]
+
+        success
       end
 
     end
