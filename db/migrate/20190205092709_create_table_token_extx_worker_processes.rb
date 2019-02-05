d165348a37b61e6a9a567be8ea59a7a7e51dadda
alpeshvmodi <alpeshvmodi@gmail.com>
2019-02-05 17:40:58 +0530

    kwc tables

---
+++ db/migrate/20190205092709_create_table_token_extx_worker_processes.rb
@@ -0,0 +1,23 @@
+class CreateTableTokenExtxWorkerProcesses < DbMigrationConnection
+  def up
+    run_migration_for_db(DbConnection::SaasSubenv) do
+      create_table :token_extx_worker_processes do |t|
+        t.column :token_id, :integer, null: false
+        t.column :tx_cron_process_id, :integer, null: true
+        t.column :token_address_id, :integer, null: false
+        t.column :properties, :tinyint, limit: 2, null: false
+      end
+
+      add_index :token_extx_worker_processes, [:token_id, :tx_cron_process_id], name: "token_cron_process_uniq", unique:true
+      add_index :token_extx_worker_processes, [:token_address_id], name: "token_address_uniq", unique:true
+    end
+  end
+
+  def down
+
+    run_migration_for_db(DbConnection::SaasSubenv) do
+      drop_table :token_extx_worker_processes
+    end
+
+  end
+end
---
+++ db/migrate/20190205100731_create_table_tx_cron_process_details.rb
@@ -0,0 +1,23 @@
+class CreateTableTxCronProcessDetails < DbMigrationConnection
+
+  def up
+    run_migration_for_db(DbConnection::SaasSubenv) do
+      create_table :tx_cron_process_details do |t|
+        t.column :cron_process_id, :integer, null: false
+        t.column :chain_id, :integer, null: false
+        t.column :queue_topic_suffix, :string, null: false
+      end
+
+      add_index :tx_cron_process_details, [:chain_id, :queue_topic_suffix], name: "chain_queue_suffix_uniq", unique:true
+    end
+  end
+
+  def down
+
+    run_migration_for_db(DbConnection::SaasSubenv) do
+      drop_table :tx_cron_process_details
+    end
+
+  end
+
+end
