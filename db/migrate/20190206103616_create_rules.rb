e8e39ef3b94d5ba975387ef5c5b54fad521fce1e
Puneet Khushwani <puneet.khushwani@ost.com>
2019-02-07 19:20:02 +0530

    change migration

--- db/migrate/20190206103616_create_rules.rb
+++ db/migrate/20190206103616_create_rules.rb
@@ -2,12 +2,13 @@ class CreateRules < DbMigrationConnection
   def up
     run_migration_for_db(DbConnection::KitSaasSubenv) do
       create_table :rules do |t|
+        t.column :token_id, :integer, default: 0
         t.column :name, :string, null: false
         t.column :kind, :tinyint, limit: 1, null:false
         t.text :abi, null: false
         t.timestamps
       end
-      add_index :rules, [:name], name: 'uk_name', unique: true
+      add_index :rules, [:token_id, :name], name: 'uk_1', unique: true
     end
   end
   
