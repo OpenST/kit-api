0a6c5b36ecf2e4321f38e56b8a3630cd5fc84ad8
Shlok Gilda <shlok@ost.com>
2019-02-13 19:41:18 +0530

    Update ip_address column to accept null values in cron processes table.

--- db/migrate/20190213133500_modify_cron_processes.rb
+++ db/migrate/20190213133500_modify_cron_processes.rb
@@ -2,12 +2,14 @@ class ModifyCronProcesses < DbMigrationConnection
   def up
     run_migration_for_db(DbConnection::SaasSubenv) do
       remove_columns :cron_processes, :chain_id
+      change_column_null :cron_processes, :ip_address, true
     end
   end
 
   def down
     run_migration_for_db(DbConnection::SaasSubenv) do
       add_column :cron_processes, :chain_id, :string, limit: 255, :null => true
+      change_column_null :cron_processes, :ip_address, false
 
     end
   end
