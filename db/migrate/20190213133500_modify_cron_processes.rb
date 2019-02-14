ad321ce69a2fe732efa40e0346d96796ce2913e3
Shlok Gilda <shlok@ost.com>
2019-02-14 13:11:42 +0530

    Updated cron processes migration.

--- db/migrate/20190213133500_modify_cron_processes.rb
+++ db/migrate/20190213133500_modify_cron_processes.rb
@@ -1,16 +1,20 @@
 class ModifyCronProcesses < DbMigrationConnection
   def up
     run_migration_for_db(DbConnection::SaasSubenv) do
+      add_column :cron_processes, :kind_name, :string, limit: 255, after: :kind, :null => true
       remove_columns :cron_processes, :chain_id
       change_column_null :cron_processes, :ip_address, true
+      add_index :cron_processes, :kind_name, name: 'uk_1', unique: true
     end
   end
 
   def down
     run_migration_for_db(DbConnection::SaasSubenv) do
+      remove_columns :cron_processes, :kind_name
       add_column :cron_processes, :chain_id, :string, limit: 255, :null => true
       change_column_null :cron_processes, :ip_address, false
 
+      remove_index :cron_processes, name: 'uk_1'
     end
   end
 end
