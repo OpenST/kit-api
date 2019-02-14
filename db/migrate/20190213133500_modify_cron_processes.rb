0ea6de89285f9996e5faebf58b39f5cbfd22c421
Shlok Gilda <shlok@ost.com>
2019-02-14 13:15:35 +0530

    Removed index.

--- db/migrate/20190213133500_modify_cron_processes.rb
+++ db/migrate/20190213133500_modify_cron_processes.rb
@@ -4,7 +4,6 @@ class ModifyCronProcesses < DbMigrationConnection
       add_column :cron_processes, :kind_name, :string, limit: 255, after: :kind, :null => true
       remove_columns :cron_processes, :chain_id
       change_column_null :cron_processes, :ip_address, true
-      add_index :cron_processes, :kind_name, name: 'uk_1', unique: true
     end
   end
 
@@ -13,8 +12,6 @@ class ModifyCronProcesses < DbMigrationConnection
       remove_columns :cron_processes, :kind_name
       add_column :cron_processes, :chain_id, :string, limit: 255, :null => true
       change_column_null :cron_processes, :ip_address, false
-
-      remove_index :cron_processes, name: 'uk_1'
     end
   end
 end
