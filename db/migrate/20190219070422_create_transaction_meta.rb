91c91497dbb2c3c1ca4ece154afa7bfe926399ce
alpeshvmodi <alpeshvmodi@gmail.com>
2019-02-19 14:59:22 +0530

    tx meta migration.

---
+++ db/migrate/20190219070422_create_transaction_meta.rb
@@ -0,0 +1,38 @@
+class CreateTransactionMeta < DbMigrationConnection
+  def up
+    run_migration_for_db(DbConnection::SaasBigSubenv) do
+
+      create_table :transaction_meta do |t|
+        t.column :associated_aux_chain_id, :integer, null: false
+        t.column :transaction_hash, :string, null: true
+        t.column :transaction_uuid, :string, null: false
+        t.column :token_id, :integer, null: false
+        t.column :status, :tinyint, limit: 1, null: false
+        t.column :kind, :tinyint, limit: 1, null: false
+        t.column :retry_count, :integer, null: true
+        t.column :next_action_at, :integer, null: true
+        t.column :lock_id, :decimal, :precision => 22, :scale => 10, null: true
+        t.column :debug_params, :text
+        t.column :sender_details, :text
+        t.timestamps
+      end
+
+      add_index :transaction_meta, [:transaction_uuid], name: 'uniq_transaction_uuid', unique: true
+      add_index :transaction_meta, [:transaction_hash, :associated_aux_chain_id], name: 'uniq_transaction_hash_chain_id', unique: true
+      add_index :transaction_meta, [:lock_id], name: 'index_lock_id'
+
+      query = "CREATE TABLE transaction_meta_archive like transaction_meta;"
+      DbConnection::SaasBigSubenv.connection.execute(query)
+
+    end
+  end
+
+  def down
+    run_migration_for_db(DbConnection::SaasBigSubenv) do
+
+      drop_table :transaction_meta
+      drop_table :transaction_meta_archive
+
+    end
+  end
+end
