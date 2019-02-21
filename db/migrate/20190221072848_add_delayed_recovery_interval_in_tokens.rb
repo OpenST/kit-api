f6ff881244589cd652188cc88dc84dc0a17bda4c
Shlok Gilda <shlok@ost.com>
2019-02-21 13:06:30 +0530

    Added delayed recovery interval column in tokens table.

---
+++ db/migrate/20190221072848_add_delayed_recovery_interval_in_tokens.rb
@@ -0,0 +1,16 @@
+class AddDelayedRecoveryIntervalInTokens < DbMigrationConnection
+  def up
+
+    run_migration_for_db(DbConnection::KitSaasSubenv) do
+      add_column :tokens, :delayed_recovery_interval, :integer, after: :decimal, null: false, default: 1
+    end
+
+  end
+
+  def down
+    run_migration_for_db(DbConnection::KitSaasSubenv) do
+      remove_column :tokens, :delayed_recovery_interval
+    end
+  end
+end
+
