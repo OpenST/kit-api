109458201fae4a0b426b774be915af2370289160
Shlok Gilda <shlok@ost.com>
2019-02-23 13:10:19 +0530

    Updated recovery_operations migration.

--- db/migrate/20190222091536_create_recovery_operations.rb
+++ db/migrate/20190222091536_create_recovery_operations.rb
@@ -7,13 +7,13 @@ class CreateRecoveryOperations < DbMigrationConnection
         t.column :user_id, :string, null: false
         t.column :kind, :tinyint, null: false
         t.column :workflow_id, :integer, null: false
-        t.column :next_action_at, :integer, null: false, default: 0
+        t.column :delayed_recovery_at, :integer, null: false, default: 0
         t.column :status, :tinyint, null: false
         t.timestamps
       end
 
       add_index :recovery_operations, [:token_id, :user_id], name: 'tid_uid_indx'
-      add_index :recovery_operations, [:next_action_at], name: 'index_next_action_at'
+      add_index :recovery_operations, [:delayed_recovery_at], name: 'index_delayed_recovery_at'
     end
   end
 
