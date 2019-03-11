5ec108030cfa0c4309f9372c187406f20b31ab8d
Dhananjay8 <dhananjay@ost.com>
2019-03-11 18:40:44 +0530

    migration for email hook restructuring.

---
+++ db/migrate/20190311120033_alter_email_service_api_call_hook.rb
@@ -0,0 +1,17 @@
+class AlterEmailServiceApiCallHook < DbMigrationConnection
+  def up
+    run_migration_for_db(DbConnection::KitSaasBigSubenv) do
+      add_column :email_service_api_call_hooks, :receiver_entity_id, :integer, after: :id, null: false
+      add_column :email_service_api_call_hooks, :receiver_entity_kind, :tinyint, limit: 1, after: :receiver_entity_id,  null: false
+      remove_column :email_service_api_call_hooks, :email
+    end
+  end
+
+  def down
+    run_migration_for_db(DbConnection::KitSaasBigSubenv) do
+      add_column :email_service_api_call_hooks, :email, :text, after: :receiver_entity_kind, null: false
+      remove_column :email_service_api_call_hooks, :receiver_entity_kind
+      remove_column :email_service_api_call_hooks, :receiver_entity_id
+    end
+  end
+end
