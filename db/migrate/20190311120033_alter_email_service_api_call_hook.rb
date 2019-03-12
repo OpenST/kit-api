76563a09f1328c163bab579b0417633c0131b8da
Dhananjay8 <dhananjay@ost.com>
2019-03-12 16:35:30 +0530

    migration for email service apicall hooks table

--- db/migrate/20190311120033_alter_email_service_api_call_hook.rb
+++ db/migrate/20190311120033_alter_email_service_api_call_hook.rb
@@ -1,5 +1,10 @@
 class AlterEmailServiceApiCallHook < DbMigrationConnection
   def up
+
+    # truncate old table
+    query = "TRUNCATE TABLE email_service_api_call_hooks;"
+    DbConnection::KitSaasBigSubenv.connection.execute(query)
+
     run_migration_for_db(DbConnection::KitSaasBigSubenv) do
       add_column :email_service_api_call_hooks, :receiver_entity_id, :integer, after: :id, null: false
       add_column :email_service_api_call_hooks, :receiver_entity_kind, :tinyint, limit: 1, after: :receiver_entity_id,  null: false
