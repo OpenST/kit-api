b117c44ab4d57fbbac6e19646cdd06898291758b
sunil <sunil@hem.com>
2019-03-15 15:34:32 +0530

    Changed personal sign message

--- lib/global_constant/message_to_sign.rb
+++ lib/global_constant/message_to_sign.rb
@@ -1,7 +1,7 @@
 module GlobalConstant
   class MessageToSign
     def self.wallet_association
-      'Please sign below to associate your account with OST'
+      'Please sign below to associate your account with OST Platform'
     end
   end
 end
