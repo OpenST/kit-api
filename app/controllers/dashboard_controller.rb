dffb554360d3c15130b3ccf99135ed513be4c70c
ankitsingh <ankit@ost.com>
2019-03-23 09:52:10 +0530

    luse cookie changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,6 +8,13 @@ class DashboardController < AuthenticationController
   #
   def get
     service_response = DashboardManagement::Get.new(params).perform
+
+    # set last used env cookie
+    set_cookie(
+      GlobalConstant::Cookie.last_used_env_cookie_name,
+      GlobalConstant::Base.main_sub_environment? ? GlobalConstant::Cookie.mainnet_env : GlobalConstant::Cookie.sandbox_env,
+      GlobalConstant::Cookie.last_used_env_cookie_expiry
+    )
     render_api_response(service_response)
   end
 
