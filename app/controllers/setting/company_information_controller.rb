bfe05e1873e41710774b0a9eaef8692edae64d5b
AnkitSingh <ankit@ost.com>
2019-04-10 17:45:27 +0530

    added redirection logic in before action

--- app/controllers/setting/company_information_controller.rb
+++ app/controllers/setting/company_information_controller.rb
@@ -2,6 +2,7 @@ class Setting::CompanyInformationController < AuthenticationController
   skip_before_action :authenticate_sub_env_access
 
   before_action :authenticate_by_password_cookie
+  before_action :perform_common_validations
 
   # Get Request for Company Information Page
   #
@@ -10,7 +11,6 @@ class Setting::CompanyInformationController < AuthenticationController
   # * Reviewed By: Kedar
   #
   def get
-    params[:luse_cookie_value] = cookies[GlobalConstant::Cookie.last_used_env_cookie_name.to_sym]
     service_response = ClientManagement::GetClientInfo.new(params).perform
 
     return render_api_response(service_response)
@@ -26,6 +26,38 @@ class Setting::CompanyInformationController < AuthenticationController
     service_response = ClientManagement::UpdateClientInfo.new(params).perform
 
     return render_api_response(service_response)
+  end
+
+  private
+
+  # Perform common validations
+  #
+  # * Author: Ankit
+  # * Date: 10/04/2019
+  # * Reviewed By:
+  #
+  def perform_common_validations
+    luse_cookie_value = cookies[GlobalConstant::Cookie.last_used_env_cookie_name.to_sym]
+    client = params[:client]
+
+    return unless client[:properties].include?(GlobalConstant::Client.has_company_info_property)
+
+    #check the cookie value here and redirect accordingly
+    if luse_cookie_value == GlobalConstant::Cookie.mainnet_env
+      #redirect to mainnet token dashboard
+      goto_screen = GlobalConstant::GoTo.mainnet_token_dashboard
+    else
+      #redirect to token dashboard
+      goto_screen = GlobalConstant::GoTo.sandbox_token_dashboard
+    end
+
+    service_response =  error_with_go_to(
+      'a_s_cm_gci_1',
+      'unauthorized_to_perform_action',
+      goto_screen
+    )
+
+    render_api_response(service_response) and return
   end
 
 end
--- app/services/client_management/get_client_info.rb
+++ app/services/client_management/get_client_info.rb
@@ -18,7 +18,6 @@ module ClientManagement
       @manager = @params[:manager]
       @client = @params[:client]
       @client_manager = @params[:client_manager]
-      @luse_cookie_value = @params[:luse_cookie_value]
 
       @api_response_data = {}
 
@@ -39,9 +38,6 @@ module ClientManagement
         r = validate
         return r unless r.success?
 
-        r = fetch_goto
-        return r unless r.success?
-
         r = fetch_sub_env_payloads
         return r unless r.success?
 
@@ -58,35 +54,6 @@ module ClientManagement
     end
 
     private
-
-    # Validate and sanitize
-    #
-    # * Author: Ankit
-    # * Date: 09/04/2019
-    # * Reviewed By: Kedar
-    #
-    # @return [Result::Base]
-    #
-    def fetch_goto
-
-      return success unless @client[:properties].include?(GlobalConstant::Client.has_company_info_property)
-
-      #check the cookie value here and redirect accordingly
-      if @luse_cookie_value == GlobalConstant::Cookie.mainnet_env
-        #redirect to mainnet token dashboard
-        goto_screen = GlobalConstant::GoTo.mainnet_token_dashboard
-      else
-        #redirect to testnet token dashboard
-        goto_screen = GlobalConstant::GoTo.sandbox_token_dashboard
-      end
-
-      error_with_go_to(
-        'a_s_cm_gci_1',
-        'unauthorized_to_perform_action',
-        goto_screen
-      )
-
-    end
 
     # fetch the sub env response data entity
     #
