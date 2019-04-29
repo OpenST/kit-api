ce5968c2fbcb3f0dc2ef4223abf6db12e0ff702e
sunil <sunil@hem.com>
2019-04-29 13:37:21 +0530

    Added test economy invite

--- app/controllers/test_economy_controller.rb
+++ app/controllers/test_economy_controller.rb
@@ -22,4 +22,15 @@ class TestEconomyController < AuthenticationController
     return render_api_response(service_response)
   end
 
+  # Invite
+  #
+  # * Author: Sunil
+  # * Date: 25/04/2019
+  # * Reviewed By: Puneet
+  #
+  def invite
+    service_response = TestEconomyManagement::Invite.new(params).perform
+    return render_api_response(service_response)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -92,6 +92,7 @@ Rails.application.routes.draw do
 
   scope "#{GlobalConstant::Environment.url_prefix}/api/test-economy", controller: 'test_economy', :format => false do
     match '' => :get, via: :GET
+    match 'invite' => :invite, via: :POST, constraints: lambda { |request| request.xhr? }
     match '' => :activate, via: :POST, constraints: lambda { |request| request.xhr? ||
         request.params[:auth_token] == GlobalConstant::Base.activate_test_economy_auth_token }
   end
