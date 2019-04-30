7be4ce4245228e86d794a46e04679e2a7e9e5fa4
Puneet Khushwani <puneet.khushwani@ost.com>
2019-04-30 16:24:48 +0530

    added max length in one go for invite email

--- app/services/test_economy_management/invite.rb
+++ app/services/test_economy_management/invite.rb
@@ -121,6 +121,13 @@ module TestEconomyManagement
           GlobalConstant::ErrorAction.default
       ) if @email_arr.blank?
 
+      return validation_error(
+          'tem_i_6',
+          'invalid_api_params',
+          ['token_demo_invite_too_many_email'],
+          GlobalConstant::ErrorAction.default
+      ) if @email_arr.length > 10
+
       success
 
     end
