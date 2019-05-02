993ee41c449e85b443104fbee239e1f52cb8560b
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-05-02 20:05:00 +0530

    Code review.

--- db/migrate/20190422140047_alter_column_conversion_rate_in_currency_conversion_rates.rb
+++ db/migrate/20190422140047_alter_column_conversion_rate_in_currency_conversion_rates.rb
@@ -1,18 +1,9 @@
 class AlterColumnConversionRateInCurrencyConversionRates < DbMigrationConnection
-  def up
+  def change
     run_migration_for_db(DbConnection::KitSaasSubenv) do
       change_table :currency_conversion_rates do |t|
         t.change(:conversion_rate, :decimal, precision: 21, scale: 10)
       end
     end
   end
-  
-  def down
-    run_migration_for_db(DbConnection::KitSaasSubenv) do
-      change_table :currency_conversion_rates do |t|
-        t.change(:conversion_rate, :decimal, precision: 15, scale: 6)
-      end
-    end
-  end
-
 end
