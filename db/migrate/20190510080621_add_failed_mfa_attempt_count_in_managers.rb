b19bde9230bc99294e8577cbbe2e3ecbc74fb358
AnaghaMurtarkar <anagha@ost.com>
2019-05-10 15:18:38 +0530

    Migration for failed mfa count.

---
+++ db/migrate/20190510080621_add_failed_mfa_attempt_count_in_managers.rb
@@ -0,0 +1,14 @@
+class AddFailedMfaAttemptCountInManagers < DbMigrationConnection
+  def up
+    run_migration_for_db(DbConnection::KitClient) do
+      add_column :managers , :failed_mfa_attempt_count, :integer, after: :failed_login_attempt_count, null: true
+    end
+  end
+
+  def down
+    run_migration_for_db(DbConnection::KitClient) do
+      remove_column :managers, :failed_mfa_attempt_count
+    end
+  end
+
+end
