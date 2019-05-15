7c91d9d584045a4be2f7375ccb22e7e7c83a7de9
shlokgilda <shlok@ost.com>
2019-05-15 15:00:31 +0530

    Updated migrations.

--- db/migrate/20190510104317_add_constants_price_oracle_address_columns_to_stake_currency.rb
+++ db/migrate/20190510104317_add_constants_price_oracle_address_columns_to_stake_currency.rb
@@ -1,15 +1,13 @@
 class AddConstantsPriceOracleAddressColumnsToStakeCurrency < DbMigrationConnection
   def up
     run_migration_for_db(DbConnection::KitSaasSubenv) do
-      add_column :stake_currencies, :price_oracle_contract_address, :string, :limit => 255, :null => true, :after => :contract_address
-      add_column :stake_currencies, :constants, :text, :null => false, :after => :price_oracle_contract_address
+      add_column :stake_currencies, :constants, :text, :null => false, :after => :contract_address
     end
   end
 
   def down
     run_migration_for_db(DbConnection::KitSaasSubenv) do
       remove_column :stake_currencies, :constants
-      remove_column :stake_currencies, :price_oracle_contract_address
     end
   end
 end
--- db/migrate/20190510121836_add_stake_currency_id_to_conversion_rates.rb
+++ db/migrate/20190510121836_add_stake_currency_id_to_conversion_rates.rb
@@ -3,16 +3,6 @@ class AddStakeCurrencyIdToConversionRates < DbMigrationConnection
     run_migration_for_db(DbConnection::KitSaasSubenv) do
       add_column :currency_conversion_rates, :stake_currency_id, :tinyint, after: :chain_id , null: true
 
-      symbolToIdHash = []
-      StakeCurrency.all.each do |row|
-        symbolToIdHash.push(row.id)
-      end
-
-      CurrencyConversionRate.all.each do |row|
-        puts row[:base_currency]
-        row[:stake_currency_id] = (row[:base_currency] == 'OST' ? symbolToIdHash[0] : symbolToIdHash[1])
-        row.save!
-      end
     end
   end
 
