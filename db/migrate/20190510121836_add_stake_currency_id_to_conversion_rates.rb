1ca57dd0a6d20452ed4cf3001884fcb9cd3d8556
alpeshvmodi <alpeshvmodi@gmail.com>
2019-05-16 12:56:11 +0530

    adding index

--- db/migrate/20190510121836_add_stake_currency_id_to_conversion_rates.rb
+++ db/migrate/20190510121836_add_stake_currency_id_to_conversion_rates.rb
@@ -3,6 +3,7 @@ class AddStakeCurrencyIdToConversionRates < DbMigrationConnection
     run_migration_for_db(DbConnection::KitSaasSubenv) do
       add_column :currency_conversion_rates, :stake_currency_id, :tinyint, after: :chain_id , null: true
 
+      add_index :currency_conversion_rates, [:chain_id, :timestamp, :status, :stake_currency_id, :quote_currency], unique: true, name: 'cuk_2'
     end
   end
 
