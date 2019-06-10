ae4a625c5eeae222f47488bf572dfc40984779ac
santhu210 <santhosh.nitdgp2015@gmail.com>
2019-06-10 16:12:06 +0530

    Revert "Merge remote-tracking branch 'origin/webhooks' into multi_pricer_cleanup"
    This reverts commit 2a5488fc96d109b2347cd7621f08aa2336ea5b3a, reversing
    changes made to 3159d36c4f51a34beedf88831a9c4bc55460f368.

--- app/models/webhook_endpoints.rb
+++
@@ -1,8 +0,0 @@
-class WebhookEndpoints < DbConnection::KitSaasSubenv
-
-  enum status: {
-    GlobalConstant::WebhookEndpoints.active => 1,
-    GlobalConstant::WebhookEndpoints.inactive => 2
-  }
-
-end
