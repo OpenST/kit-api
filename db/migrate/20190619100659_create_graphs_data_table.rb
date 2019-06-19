a51dbe8dbaa409d266ecd3bd99a6f912fbdc3977
AnkitSingh <ankit@ost.com>
2019-06-19 17:35:13 +0530

    minor changes

--- db/migrate/20190619100659_create_graphs_data_table.rb
+++ db/migrate/20190619100659_create_graphs_data_table.rb
@@ -1,7 +1,7 @@
 class CreateGraphsDataTable < DbMigrationConnection
   def up
     run_migration_for_db(DbConnection::KitSaasSubenv) do
-      create_table :graphs_data do |t|
+      create_table :graph_data do |t|
         t.column :token_id, :int, null: false
         t.column :graph_type, :tinyint, null: false
         t.column :duration_type, :tinyint, null: false
@@ -9,14 +9,14 @@ class CreateGraphsDataTable < DbMigrationConnection
         t.timestamps
       end
 
-      add_index :graphs_data, [:token_id, :graph_type, :duration_type], name: 'uk_1'
+      add_index :graph_data, [:token_id, :graph_type, :duration_type], name: 'uk_1'
     end
   end
 
   def down
     run_migration_for_db(DbConnection::KitSaasSubenv) do
-      remove_index :graphs_data, name: 'uk_1'
-      drop_table :graphs_data if DbConnection::KitSaasSubenv.connection.table_exists? :graphs_data
+      remove_index :graph_data, name: 'uk_1'
+      drop_table :graph_data if DbConnection::KitSaasSubenv.connection.table_exists? :graph_data
     end
   end
 end
--- lib/global_constant/graph_constants.rb
+++ lib/global_constant/graph_constants.rb
@@ -4,7 +4,8 @@ module GlobalConstant
   class GraphConstants
 
     class << self
-      
+
+      #Duration Type Start
       def duration_type_day
         'day'
       end
@@ -20,6 +21,21 @@ module GlobalConstant
       def duration_type_year
         'year'
       end
+      #Duration Type End
+
+      #Graph Type Start
+      def total_transactions
+        'total_transactions'
+      end
+
+      def total_transactions_by_name
+        'total_transactions_by_name'
+      end
+
+      def total_transactions_by_type
+        'total_transactions_by_type'
+      end
+      #Graph Type End
 
     end
 
