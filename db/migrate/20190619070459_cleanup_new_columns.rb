1ef765f181de8449affb1350f6dbae5473327f7d
santhu210 <santhosh.nitdgp2015@gmail.com>
2019-06-19 13:57:21 +0530

    Minor change

--- db/migrate/20190619070459_cleanup_new_columns.rb
+++ db/migrate/20190619070459_cleanup_new_columns.rb
@@ -3,7 +3,7 @@ class CleanupNewColumns < DbMigrationConnection
     run_migration_for_db(DbConnection::KitSaasSubenv) do
       change_column :currency_conversion_rates, :stake_currency_id, :tinyint, null: false
       change_column :currency_conversion_rates, :quote_currency_id, :tinyint, null: false
-      #remove_column :currency_conversion_rates, :quote_currency
+      remove_column :currency_conversion_rates, :quote_currency
     end
   end
 
