36310012d5d2ac448a0cbae09fb9ab889b79be9e
santhu210 <santhosh.nitdgp2015@gmail.com>
2019-07-26 13:45:18 +0530

    Job for handling post signup sandbox steps

---
+++ app/jobs/post_signup_sandbox_tasks_job.rb
@@ -0,0 +1,89 @@
+class PostSignupSandboxTasksJob < ApplicationJob
+
+  queue_as GlobalConstant::Sidekiq.queue_name :default_medium_priority_queue
+
+  # Perform
+  #
+  # * Author: Santhosh
+  # * Date: 26/07/2019
+  # * Reviewed By:
+  #
+  # @param [Hash] manager_id (mandatory) - manager id
+  # @param [Hash] client_id (mandatory) - client id
+  #
+  def perform(params)
+
+    init_params(params)
+
+    @attributes_hash = fetch_attributes
+
+    update_contact_in_email_service
+
+    notify_devs
+
+  end
+
+  # init params
+  #
+  # * Author: Santhosh
+  # * Date: 26/07/2019
+  # * Reviewed By:
+  #
+  # @param [Hash] params
+  #
+  def init_params(params)
+    @manager_id = params[:manager_id].to_i
+    @client_id = params[:client_id].to_i
+
+    @token_name = nil
+    @testnet_view_link = nil
+    @failed_logs = {}
+
+    @manager = CacheManagement::Manager.new([@manager_id]).fetch[@manager_id]
+  end
+
+  # fetch attributes - token name, testnet view link
+  #
+  # * Author: Santhosh
+  # * Date: 26/07/2019
+  # * Reviewed By:
+  #
+  # @returns [Hash]
+  #
+  def fetch_attributes
+    client_mile_stone = ClientMileStone.new({ client_id: @client_id })
+    client_mile_stone.add_extra_attributes
+  end
+
+  # Update contact in Pepo Campaigns
+  #
+  # * Author: Santhosh
+  # * Date: 26/07/2019
+  # * Reviewed By:
+  #
+  def update_contact_in_email_service
+
+    r = Email::HookCreator::UpdateContact.new(
+        receiver_entity_id: @manager_id,
+        receiver_entity_kind: GlobalConstant::EmailServiceApiCallHook.manager_receiver_entity_kind,
+        custom_attributes: @attributes_hash
+    ).perform
+
+    @failed_logs[@manager_id] = r.to_hash unless r.success?
+  end
+
+  # Send mail
+  #
+  # * Author: Santhosh
+  # * Date: 26/07/2019
+  # * Reviewed By:
+  #
+  def notify_devs
+    ApplicationMailer.notify(
+        data: @failed_logs,
+        body: { manager_id: @manager_id },
+        subject: 'Exception in Post signup sandbox tasks job'
+    ).deliver if @failed_logs.present?
+  end
+
+end
