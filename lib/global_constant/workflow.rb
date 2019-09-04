b7214c350e496f7ba66ae494a0160a32259778c2
shlokgilda <shlok@ost.com>
2019-09-04 11:57:18 +0530

    Updated workflow constants.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- lib/global_constant/workflow.rb
+++ lib/global_constant/workflow.rb
@@ -24,6 +24,66 @@ module GlobalConstant
         'grant_eth_stake_currency'
       end
 
+      def setup_user
+        'setup_user'
+      end
+
+      def test
+        'test'
+      end
+
+      def authorize_device
+        'authorize_device'
+      end
+
+      def authorize_session
+        'authorize_session'
+      end
+
+      def revoke_device
+        'revoke_device'
+      end
+
+      def revoke_session
+        'revoke_session'
+      end
+
+      def initiate_recovery
+        'initiate_recovery'
+      end
+
+      def abort_recovery_by_owner
+        'abort_recovery_by_owner'
+      end
+
+      def reset_recovery_owner
+        'reset_recovery_owner'
+      end
+
+      def execute_recovery
+        'execute_recovery'
+      end
+
+      def abort_recovery_by_recovery_controller
+        'abort_recovery_by_recovery_controller'
+      end
+
+      def logout_sessions
+        'logout_sessions'
+      end
+
+      def st_prime_redeem_and_unstake
+        'st_prime_redeem_and_unstake'
+      end
+
+      def bt_redeem_and_unstake
+        'bt_redeem_and_unstake'
+      end
+
+      def update_price_point
+        'update_price_point'
+      end
+
       # Workflow kind constants End #
 
       def in_progress
