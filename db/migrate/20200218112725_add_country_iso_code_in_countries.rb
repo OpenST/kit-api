edc75585770ddaf3858903e2977cb6f6622c4237
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2020-02-18 18:54:10 +0530

    Added index on countries table.

--- db/migrate/20200218112725_add_country_iso_code_in_countries.rb
+++ db/migrate/20200218112725_add_country_iso_code_in_countries.rb
@@ -2,12 +2,14 @@ class AddCountryIsoCodeInCountries < DbMigrationConnection
   def up
     run_migration_for_db(DbConnection::KitSaasRedemptionSubenv) do
       add_column :countries ,:country_iso_code, :string, limit: 3, null: false, after: :name
+      add_index :countries, [:country_iso_code], unique: true, name: 'uk_1'
     end
   end
 
   def down
-    run_migration_for_db(DbConnection::SaasBigSubenv) do
-      remove_column :country_iso_code
+    run_migration_for_db(DbConnection::KitSaasRedemptionSubenv) do
+      remove_index :countries, name: 'uk_1'
+      remove_column :countries, :country_iso_code
     end
   end
 end
