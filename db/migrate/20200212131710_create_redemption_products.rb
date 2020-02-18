8185ddb44df779f1f82b34fedcc7e8b55a6ecfb6
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2020-02-18 11:44:29 +0530

    Minor changes.

--- db/migrate/20200212131710_create_redemption_products.rb
+++ db/migrate/20200212131710_create_redemption_products.rb
@@ -5,8 +5,6 @@ class CreateRedemptionProducts < DbMigrationConnection
         t.column :name, :string, limit: 255, null: false
         t.column :description, :string, limit: 1000, null: false
         t.column :image, :text, null: true
-        t.column :denomination, :text, null: false
-        t.column :expiry_in_days, :int, null: false
         t.column :status, :integer, limit: 1, null: false
         t.timestamps
       end
